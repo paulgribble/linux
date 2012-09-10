52b0ffa58b21cd38b60e7a1c771c63cb8cf590fb
Paul Gribble <paul@gribblelab.org>
2012-09-09 23:27:38 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,16 +7,19 @@ sudo apt-get install git
 git config --global user.name "Paul Gribble"
 git config --global user.email paul@gribblelab.org
 
+sudo apt-get install emacs emacs-goodies-el
+
 sudo apt-get install auctex r-base ess
 #sudo apt-get install python-scipy python-numpy python-matplotlib python-setuptools ipython
 sudo apt-get install ntp build-essential libgsl0-dev plotutils gnuplot ssh gtk2-engines-pixbuf
 sudo apt-get install octave gnome-session ruby rubygems sane python-gpgme
 
-sudo apt-get install emacs emacs-goodies-el texlive-full texlive-fonts-extra cm-super texlive-latex-extra sudo apt-get install ttf-mscorefonts-installer
+sudo apt-get install texlive-full texlive-fonts-extra cm-super texlive-latex-extra
 sudo texconfig paper letter
 wget http://www.tug.org/fonts/getnonfreefonts/install-getnonfreefonts
 sudo ./install-getnonfreefonts
 sudo getnonfreefonts garamond
+sudo apt-get install ttf-mscorefonts-installer
 
 cd ..
 git clone git://orgmode.org/org-mode.git
