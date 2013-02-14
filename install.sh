09bb2fdc92f9cfb237a2fac6a38e5b7a1a9abf9f
Paul Gribble <paul@gribblelab.org>
2013-02-14 12:05:09 -0500

    misc

--- install.sh
+++ install.sh
@@ -50,7 +50,7 @@ sudo apt-get -qq install libgsl0-dev plotutils gnuplot
 
 # misc system utils
 echo "installing system utils"
-sudo apt-get -qq install gnome-session sane ssh
+sudo apt-get -qq install gnome-session sane ssh indicator-multiload wireshark
 
 # LaTeX
 echo "installing LaTeX"
