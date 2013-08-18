9d951f1575c7a6506d8b091656c1e6c1a602a8a4
Paul Gribble <paul@gribblelab.org>
2013-08-18 09:45:41 -0400

    added debian_install.sh

---
+++ debian_install.sh
@@ -0,0 +1,73 @@
+# emacs
+echo ""
+echo "installing emacs"
+apt-get -qq install emacs emacs-goodies-el
+
+# developer tools
+echo ""
+echo "installing developer tools"
+apt-get -qq install build-essential
+
+# linux kernel headers
+apt-get -qq install linux-headers-$(uname -r)
+
+# git source control
+echo ""
+echo "installing git"
+apt-get -qq install git
+
+# R statistical package
+echo ""
+echo "installing R"
+apt-get -qq install r-base ess
+
+# octave
+echo ""
+echo "installing octave"
+apt-get -qq install octave
+
+# gsl, plotutils, gnuplot
+echo ""
+echo "installing gnu scientific library, plotutils, gnuplot"
+apt-get -qq install libgsl0-dev plotutils gnuplot
+
+# pandoc document conversion
+echo ""
+echo "installing pandoc"
+apt-get -qq install pandoc
+
+# LaTeX document typesetting system
+echo ""
+echo "installing LaTeX"
+apt-get -qq install texlive-full texlive-fonts-extra cm-super texlive-latex-extra auctex texinfo
+texconfig paper letter
+mktexlsr
+
+# fonts
+echo ""
+echo "installing a bunch of fonts"
+wget http://www.tug.org/fonts/getnonfreefonts/install-getnonfreefonts
+chmod +x install-getnonfreefonts
+./install-getnonfreefonts
+getnonfreefonts garamond
+mktexlsr
+apt-get -qq install ttf-mscorefonts-installer
+apt-get -qq install xfonts-100dpi xfonts-75dpi
+
+# python scientific stack
+echo ""
+echo "installing python scientific stack"
+sudo apt-get -qq install python-sip pyqt4-dev-tools
+sudo apt-get -qq install python-numpy python-scipy python-matplotlib ipython ipython-doc ipython-notebook ipython-qtconsole python-sympy python-virtualenv python-dev python-pip
+sudo apt-get -qq install python-pygame
+
+# generate ssh key
+echo ""
+echo "generating ssh key"
+echo "just hit <ENTER> when asked questions, until the procedure stops"
+/bin/su - scicomp -c "ssh-keygen -t rsa"
+
+# done
+echo ""
+echo "DONE!"
+
