ddacb482ffa51f8d4df111f19d2a3b57ad3672b5
Paul Gribble <paul@gribblelab.org>
2013-08-21 12:20:44 -0400

    updates

--- debian_install.sh
+++ debian_install.sh
@@ -1,50 +1,75 @@
-# emacs
-echo ""
-echo "installing emacs"
-apt-get -qq install emacs emacs-goodies-el
+#!/bin/sh
 
-# org-mode
-echo ""
-echo "installing orgmode"
-apt-get -qq install org-mode
+# FIRST you will need to do the following:
+# 1. open a ROOT terminal
+# type adduser <yourusername> sudo
+#   (where <yourusername> is your short username)
+#   (if you don't know it, open a regular terminal and
+#    type whoami)
+# 2. logout
+# 3. log back in
+# 4. open a regular terminal and start this script by typing
+#    /bin/sh debian_install.sh
+# if you already have sudo priviledges on your username you
+# can skip steps 1, 2, 3
 
-# developer tools
-echo ""
-echo "installing developer tools"
-apt-get -qq install build-essential
-
-# linux kernel headers
-apt-get -qq install linux-headers-$(uname -r)
+echo "updating"
+sudo apt-get -qq update
+sudo apt-get -qq upgrade
 
 # git source control
 echo ""
 echo "installing git"
-apt-get -qq install git
+sudo apt-get -qq install git
+
+# developer tools
+echo ""
+echo "installing developer tools"
+sudo apt-get -qq install build-essential
+
+# linux kernel headers
+echo ""
+echo "installing linux kernel headers"
+sudo apt-get -qq install linux-headers-$(uname -r)
+
+# emacs
+echo ""
+echo "installing emacs"
+sudo apt-get -qq install emacs emacs-goodies-el
+# more emacs stuff
+sudo cp org-exp-bibtex.el /usr/share/emacs/site-lisp/
+sudo apt-get install -qq bibtex2html
+cp dotemacs ~/.emacs
+
+# org-mode
+echo ""
+echo "installing orgmode"
+sudo apt-get -qq install org-mode
 
 # R statistical package
 echo ""
 echo "installing R"
-apt-get -qq install r-base ess
+sudo apt-get -qq install r-base ess
 
 # octave
 echo ""
 echo "installing octave"
-apt-get -qq install octave
+sudo apt-get -qq install octave
 
 # gsl, plotutils, gnuplot
 echo ""
 echo "installing gnu scientific library, plotutils, gnuplot"
-apt-get -qq install libgsl0-dev plotutils gnuplot
+sudo apt-get -qq install libgsl0-dev plotutils gnuplot
 
 # pandoc document conversion
 echo ""
 echo "installing pandoc"
-apt-get -qq install pandoc
+sudo apt-get -qq install pandoc
 
 # LaTeX document typesetting system
 echo ""
 echo "installing LaTeX"
-apt-get -qq install texlive-full texlive-fonts-extra cm-super texlive-latex-extra auctex texinfo
+sudo apt-get -qq install texlive-full texlive-fonts-extra cm-super texlive-latex-extra auctex texinfo
 texconfig paper letter
 mktexlsr
 
@@ -56,8 +81,8 @@ chmod +x install-getnonfreefonts
 ./install-getnonfreefonts
 getnonfreefonts garamond
 mktexlsr
-apt-get -qq install ttf-mscorefonts-installer
-apt-get -qq install xfonts-100dpi xfonts-75dpi
+sudo apt-get -qq install ttf-mscorefonts-installer
+sudo apt-get -qq install xfonts-100dpi xfonts-75dpi
 
 # python scientific stack
 echo ""
@@ -66,11 +91,32 @@ sudo apt-get -qq install python-sip pyqt4-dev-tools
 sudo apt-get -qq install python-numpy python-scipy python-matplotlib ipython ipython-doc ipython-notebook ipython-qtconsole python-sympy python-virtualenv python-dev python-pip
 sudo apt-get -qq install python-pygame
 
+# tor
+echo ""
+echo "installing tor"
+sudo apt-get -qq install tor
+
 # generate ssh key
 echo ""
 echo "generating ssh key"
 echo "just hit <ENTER> when asked questions, until the procedure stops"
-/bin/su - scicomp -c "ssh-keygen -t rsa"
+ssh-keygen -t rsa
+
+# laptop?
+while true; do
+    read -p "install laptop-mode-tools? (y/n) " yn
+    case $yn in
+        [Yy]* ) echo ""; echo "installing laptop-mode-tools"; sudo apt-get -qq install laptop-mode-tools; break;;
+        [Nn]* ) exit;;
+        * ) echo "please answer y or n.";;
+    esac
+done
+
+# update reps and upgrade any leftovers
+echo ""
+echo "update & upgrade"
+sudo apt-get -qq update
+sudo apt-get -qq upgrade
 
 # done
 echo ""
--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -1,97 +1,127 @@
 #!/bin/sh
 
-echo "updating ubuntu"
+# FIRST you will need to do the following:
+# 1. open a ROOT terminal
+# type adduser <yourusername> sudo
+#   (where <yourusername> is your short username)
+#   (if you don't know it, open a regular terminal and
+#    type whoami)
+# 2. logout
+# 3. log back in
+# 4. open a regular terminal and start this script by typing
+#    /bin/sh ubuntu_install.sh
+# if you already have sudo priviledges on your username you
+# can skip steps 1, 2, 3
+
+echo ""
+echo "updating"
 sudo apt-get -qq update
 sudo apt-get -qq upgrade
 
 # git
+echo ""
 echo "installing git"
 sudo apt-get -qq install git
 git config --global user.name "Paul Gribble"
 git config --global user.email paul@gribblelab.org
 
 # development
+echo ""
 echo "installing development tools"
 sudo apt-get -qq install build-essential
 
 # kernel source
-echo "installing kernel headers"
+echo ""
+echo "installing linux kernel headers"
 sudo apt-get -qq install linux-headers-$(uname -r)
 
 # emacs
 echo "installing emacs"
-sudo apt-get -qq install emacs emacs-goodies-el texinfo
-
-# org-mode
-echo "installing emacs org-mode"
-cd ..
-git clone git://orgmode.org/org-mode.git
-cd org-mode
-make
-make doc
-sudo make install
-cd ..
-cd ubuntu
+sudo apt-get -qq install emacs emacs-goodies-el
+# more emacs stuff
 sudo cp org-exp-bibtex.el /usr/share/emacs/site-lisp/
 sudo apt-get install -qq bibtex2html
 cp dotemacs ~/.emacs
 
+# org-mode
+echo ""
+echo "installing orgmode"
+apt-get -qq install org-mode
+
 # R
+echo ""
 echo "installing R"
 sudo apt-get -qq install r-base ess
 
-# misc
-echo "installing ntp and gtk2-engines-pixbuf"
-sudo apt-get -qq install ntp gtk2-engines-pixbuf
-
 # octave
+echo ""
 echo "installing octave"
 sudo apt-get -qq install octave
 
 # gsl, plotting stuff
+echo ""
 echo "installing gsl and plotting stuff"
 sudo apt-get -qq install libgsl0-dev plotutils gnuplot
 
-# misc system utils
-echo "installing system utils"
-sudo apt-get -qq install gnome-session-fallback sane ssh wireshark
+# pandoc document conversion
+echo ""
+echo "installing pandoc"
+sudo apt-get -qq install pandoc
 
 # LaTeX
+echo ""
 echo "installing LaTeX"
-sudo apt-get -qq install texlive-full texlive-fonts-extra cm-super texlive-latex-extra auctex
+sudo apt-get -qq install texlive-full texlive-fonts-extra cm-super texlive-latex-extra auctex texinfo
 sudo texconfig paper letter
 echo "mktexlsr"
 sudo mktexlsr
-echo "installing non-free-fonts"
+
+# fonts
+echo ""
+echo "installing a bunch of fonts"
 wget http://www.tug.org/fonts/getnonfreefonts/install-getnonfreefonts
 chmod +x install-getnonfreefonts
 sudo ./install-getnonfreefonts
 sudo getnonfreefonts garamond
 sudo mktexlsr
 sudo apt-get -qq install ttf-mscorefonts-installer
-
-# Pandoc
-sudo apt-get -qq install pandoc
+sudo apt-get -qq install xfonts-100dpi xfonts-75dpi
 
 # python scientific stack
+echo ""
 echo "installing python scientific stack"
 sudo apt-get -qq install python-sip pyqt4-dev-tools
 sudo apt-get -qq install python-numpy python-scipy python-matplotlib ipython ipython-doc ipython-notebook ipython-qtconsole python-sympy python-virtualenv python-dev python-pip
 sudo apt-get -qq install python-pygame
 
 # tor
+echo ""
+echo "installing tor"
 sudo apt-get -qq install tor
 
-# fonts
-sudo apt-get -qq install xfonts-100dpi xfonts-75dpi
-echo "you will need to logout and log back in to activate new fonts"
+# generate ssh key
+echo ""
+echo "generating ssh key"
+echo "just hit <ENTER> when asked questions, until the procedure stops"
+/bin/su - scicomp -c "ssh-keygen -t rsa"
 
 # laptop?
-# sudo apt-get install laptop-mode-tools
+while true; do
+    read -p "install laptop-mode-tools? (y/n) " yn
+    case $yn in
+        [Yy]* ) echo ""; echo "installing laptop-mode-tools"; sudo apt-get -qq install laptop-mode-tools; break;;
+        [Nn]* ) exit;;
+        * ) echo "please answer y or n.";;
+    esac
+done
 
 # update reps and upgrade any leftovers
+echo ""
+echo "update & upgrade"
 sudo apt-get -qq update
 sudo apt-get -qq upgrade
 
-echo "done"
+# done
+echo ""
+echo "DONE!"
 
