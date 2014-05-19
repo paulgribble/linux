47b51e6344059f84258264df0eb92c44c6bf4189
Paul Gribble <paul@gribblelab.org>
2014-05-19 17:26:41 -0400

    chrome

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -122,7 +122,7 @@ sudo apt-get -qq upgrade
 echo ""
 echo "installing google-chrome"
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
-sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 sudo apt-get update 
 sudo apt-get -qq install google-chrome-stable
 
